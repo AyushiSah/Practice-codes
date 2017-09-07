ca3321aa38f3e1cdf6f1d3562fcc9432a2dc2ad2
AyushiSah <31668848+AyushiSah@users.noreply.github.com>
2017-09-07 19:31:56 +0530

    Add files via upload

---
+++ Creating a matrix.R
@@ -0,0 +1,8 @@
+#Making a matrix with 3 rows and 4 columns where each value is assigned to 0
+matrix(0,3,4)
+#Using a vector to initialise the matrix's values from 1 to 12
+a<-1:12
+print(a)
+
+#Calling the matrix with the vector to replace the matrix's values from 0 to vector's values
+matrix(a,3,4)
